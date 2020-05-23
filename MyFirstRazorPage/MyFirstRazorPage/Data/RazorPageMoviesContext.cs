bb21d9646f6604664b734d600f7ddb9252e99914
anhphuong0204 <phuong.nguyen02042000@hcmut.edu.vn>
2020-05-23 18:10:30 +0700

    Add scaffolded Razor pages (CRUD methods)

---
+++ MyFirstRazorPage/MyFirstRazorPage/Data/RazorPageMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyFirstRazorPage.Models;
+
+namespace MyFirstRazorPage.Data
+{
+    public class RazorPageMoviesContext : DbContext
+    {
+        public RazorPageMoviesContext (DbContextOptions<RazorPageMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyFirstRazorPage.Models.Movie> Movie { get; set; }
+    }
+}
--- MyFirstRazorPage/MyFirstRazorPage/Models/Movie.cs
+++ MyFirstRazorPage/MyFirstRazorPage/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace MyFirstRazorPage.Models
 {
     public class Movie
     {
+        public int ID { get; set; }
+        public string Tittle { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseTime { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
     }
 }
