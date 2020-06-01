54d2cd00ba72ab80e45808608c32d58a55e9a5b6
anhphuong0204 <phuong.nguyen02042000@hcmut.edu.vn>
2020-06-01 17:36:50 +0700

    Create a list view of Comic Books with TeamTreeHouse

---
+++ ComicBookgallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ComicBookGallery.Models;
+using ComicBookGallery.Data;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ComicBooksController()
+        {
+            comicRepository = new ComicRepository();
+        }
+
+        private ComicRepository comicRepository;
+
+        public ActionResult Index()
+        {
+            return View(comicRepository.GetComicBooks());
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+            ComicBook comicBook = comicRepository.GetComicBook(id.Value);
+            if (comicBook == null)
+                return HttpNotFound();
+            return View(comicBook);
+        }
+    }
+}
